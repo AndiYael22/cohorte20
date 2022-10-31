e2132aef7204f631874becaa1579445f76edd3d7
andi <andiyael2208@gmail.com>
2022-10-31 17:31:58 -0600

    javascript+tarea

---
+++ intro-web-7/scrip.js
@@ -0,0 +1,86 @@
+/* console.log("ho")
+alert("alerta"); */
+var num=1243;
+var pal="palabra";
+var bool=true;
+
+var nu=null;
+var andi=1998;
+var act=2022;
+var alan= 1996;
+var david=1993;
+var ana =1998; 
+var oli=1992;
+var montiel=1999;
+
+console.log("mi año de nacimiento: ",act-andi);
+console.log("año de nacimiento de alan: ", act-alan);
+console.log("año de nacimiento de david: ", act-david);
+console.log("año de nacimiento de ana: ", act-ana);
+console.log("año de nacimiento de olivia: ", act-oli);
+console.log("año de nacimiento de montiel: ", act-montiel);
+//false
+console.log(Number(false));
+console.log(String(false));
+console.log(Boolean(false));
+//true
+console.log(Number(true));
+console.log(String(true));
+console.log(Boolean(true));
+//0
+console.log(Number(0));
+console.log(String(0));
+console.log(Boolean(0));
+//1
+console.log(Number(1));
+console.log(String(1));
+console.log(Boolean(1));
+
+//"0"
+console.log(Number("0"));
+console.log(String("0"));
+console.log(Boolean("0"));
+
+//"000"
+console.log(Number("000"));
+console.log(String("000"));
+console.log(Boolean("000"));
+//"1"
+console.log(Number("1"));
+console.log(String("1"));
+console.log(Boolean("1"));
+//NaN
+console.log(Number(NaN));
+console.log(String(NaN));
+console.log(Boolean(NaN));
+
+//Infinity
+console.log(Number(Infinity));
+console.log(String(Infinity));
+console.log(Boolean(Infinity));
+//-Infinity
+console.log(Number(-Infinity));
+console.log(String(-Infinity));
+console.log(Boolean(-Infinity));
+// ""
+console.log(Number(""));
+console.log(String(""));
+console.log(Boolean(""));
+//20
+console.log(Number("20"));
+console.log(String("20"));
+console.log(Boolean("20"));
+//Twenty
+console.log(Number("Twenty"));
+console.log(String("Twenty"));
+console.log(Boolean("Twenty"));
+
+//null
+console.log(Number(null));
+console.log(String(null));
+console.log(Boolean(null));
+
+//unefined
+console.log(Number(undefined));
+console.log(String(undefined));
+console.log(Boolean(undefined));
